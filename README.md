a035a5f9c102b23276a918f79ffc18fc4b933b5f
Iva-n Vasylynyuk <jvin08@gmail.com>
2022-12-05 14:26:18 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+https://unsplash.com/oauth/applications data base
+
+https://api.unsplash.com/photos/?client_id=YOUR_ACCESS_KEY
+https://api.unsplash.com/photos/
---
+++ src/Photo.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Photo = ({
+  urls: { regular },
+  likes,
+  alt_description,
+  user: {
+    name,
+    portfolio_url,
+    profile_image: { medium } }
+}) => {
+  return <article className='photo'>
+    <img src={regular} alt={alt_description} />
+    <div className='photo-info'>
+      <div>
+        <h4>{alt_description}</h4>
+        <p>{likes} likes</p>
+      </div>
+      <a href={portfolio_url}>
+        <img src={medium} alt={name} className='user-img'/>
+      </a>
+    </div>
+  </article>
+}
+
+export default Photo
